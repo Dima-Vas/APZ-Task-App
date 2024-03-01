a2de594b3b939c2ec0488cdc209b7996f8a0b38c
Dima-Vas <dmytro.vasylkiv@gmail.com>
2024-03-01 21:27:38 +0200

    IQueue implemented

--- src/main/java/com/lab2/hazelcast/app1/HazelcastApplication.java
+++ src/main/java/com/lab2/hazelcast/app1/HazelcastApplication.java
@@ -4,52 +4,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+import com.hazelcast.collection.IQueue;
 import com.hazelcast.config.Config;
+import com.hazelcast.config.QueueConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.map.IMap;
 
 @SpringBootApplication
 @PropertySource(value = { "hazel/first.properties","application.properties" })
 public class HazelcastApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(HazelcastApplication.class, args);
         Config helloWorldConfig = new Config();
         helloWorldConfig.setClusterName("hello-world");
+
+        QueueConfig qc = helloWorldConfig.getQueueConfig("myQueue");
+        qc.setMaxSize(10);
         HazelcastInstance hz = Hazelcast.newHazelcastInstance(helloWorldConfig);
 
-
-        String key = "key";
-        IMap<String, Integer> map = hz.getMap("my-distributed-map");
-        map.putIfAbsent(key, 0);
-
-        for (int i = 0; i < 1000; ++i) {
-            if ( i % 100 == 0 ) System.out.println( "At: " + i );
-            // map.lock( key );
-            // try {
-            //     Integer value = map.get( key );
-            //     Thread.sleep( 10 );
-            //     value++;
-            //     map.put( key, value );
-            // } catch (InterruptedException e) {
-            //     e.printStackTrace();
-            // } finally {
-            //     map.unlock( key );
-            // }
-            for (; ; ) {
-                Integer oldValue = map.get( key );
-                @SuppressWarnings("removal")
-                Integer newValue = new Integer( oldValue );
-                try {
-                    Thread.sleep( 10 );
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                newValue++;
-                if ( map.replace( key, oldValue, newValue ) )
-                    break;
+        IQueue<Integer> queue = hz.getQueue( "myQueue" );
+        while ( true ) {
+            int item = queue.take(); 
+            System.out.println( "Consumed: " + item );
+            if ( item == -1 ) {
+                queue.put( -1 );
+                break;
             }
+            Thread.sleep( 5000 ); 
         }
-        System.out.println("From app #1 : " + map.get(key));
+
     }
 }
--- src/main/java/com/lab2/hazelcast/app3/HazelcastApplication3.java
+++ src/main/java/com/lab2/hazelcast/app3/HazelcastApplication3.java
@@ -4,52 +4,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+import com.hazelcast.collection.IQueue;
 import com.hazelcast.config.Config;
+import com.hazelcast.config.QueueConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.map.IMap;
 
 @SpringBootApplication
 @PropertySource(value = {"hazel/third.properties", "application.properties" })
 public class HazelcastApplication3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(HazelcastApplication3.class, args);
         Config helloWorldConfig = new Config();
         helloWorldConfig.setClusterName("hello-world");
+
+        QueueConfig qc = helloWorldConfig.getQueueConfig("myQueue");
+        qc.setMaxSize(10);
         HazelcastInstance hz = Hazelcast.newHazelcastInstance(helloWorldConfig);
 
-        IMap<String, Integer> map = hz.getMap("my-distributed-map");
-
-        String key = "key";
-        map.putIfAbsent(key, 0);
-
-        for (int i = 0; i < 1000; ++i) {
-            if ( i % 100 == 0 ) System.out.println( "At: " + i );
-            // map.lock( key );
-            // try {
-            //     Integer value = map.get( key );
-            //     Thread.sleep( 10 );
-            //     value++;
-            //     map.put( key, value );
-            // } catch (InterruptedException e) {
-            //     e.printStackTrace();
-            // } finally {
-            //     map.unlock( key );
-            // }
-            for (; ; ) {
-                Integer oldValue = map.get( key );
-                @SuppressWarnings("removal")
-                Integer newValue = new Integer( oldValue );
-                try {
-                    Thread.sleep( 10 );
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                newValue++;
-                if ( map.replace( key, oldValue, newValue ) )
-                    break;
+        IQueue<Integer> queue = hz.getQueue( "myQueue" );
+        while ( true ) {
+            int item = queue.take(); 
+            System.out.println( "Consumed: " + item );
+            if ( item == -1 ) {
+                queue.put( -1 );
+                break;
             }
+            Thread.sleep( 5000 ); 
         }
-        System.out.println("From app #3 : " + map.get(key));
     }
 }
